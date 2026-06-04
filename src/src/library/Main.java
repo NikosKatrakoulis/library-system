112660627da90238c75bb43acb1d85be31f4ca6d
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-06-04 08:31:59 +0300

    feat: add Main class with CLI menu

--- src/src/library/Main.java
+++ src/src/library/Main.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        try (Scanner scanner = new Scanner(System.in)){
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
 
             Library library = new Library();
 
@@ -18,32 +17,53 @@ public class Main {
                 System.out.println("2. List Books");
                 System.out.println("3. Search by Title");
                 System.out.println("0. Exit");
-                System.out.println("Choose one: ");
+                System.out.print("Choose one: ");
                 String menuInput = scanner.nextLine().trim().toLowerCase();
-                int menuChoice = Integer.parseInt(menuInput);
 
-                switch (menuChoice) {
-                    case 1:
-                        library.addBook();
-                        break;
-                    case 2:
-                        library.listBooks();
-                        break;
-                    case 3:
-                        library.searchByTitle();
-                        break;
-                    case 0:
-                        isRunning = false;
-                        break;
-                    default:
-                        System.out.println("Please enter a number between 0 and 3!");
-                        System.out.println();
-                        break;
+
+                try {
+                    int menuChoice = Integer.parseInt(menuInput);
+
+                    switch (menuChoice) {
+                        case 1:
+                            System.out.print("Enter the isbn: ");
+                            String isbn = scanner.nextLine();
+                            System.out.print("Enter the title: ");
+                            String title = scanner.nextLine();
+                            System.out.print("Enter the author:");
+                            String author = scanner.nextLine();
+                            System.out.println();
+                            Book book = new Book(isbn, title, author);
+                            System.out.println("You added the book: " + "\n" + book);
+                            library.addBook(book);
+                            break;
+                        case 2:
+                            System.out.println("=== List of Books ===");
+                            library.listBooks();
+                            break;
+                        case 3:
+                            System.out.print("Enter the title of the book: ");
+                            String bookTitle = scanner.nextLine();
+                            Book result = library.searchByTitle(bookTitle);
+                            if (result == null) {
+                                System.out.println("Book not found.");
+                            } else {
+                                System.out.println(result);
+                            }
+                            break;
+                        case 0:
+                            isRunning = false;
+                            break;
+                        default:
+                            System.out.println("Please enter a number between 0 and 3!");
+                            System.out.println();
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a number!");
+                    System.out.println();
                 }
-
             }
-
-
         }
     }
 }
